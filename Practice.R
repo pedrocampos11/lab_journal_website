9e6a8b0a46dc2f2c68211ed9ce41348404d6616a
pedrocampos11 <pedro.campos.chavez@tuhh.de>
2021-04-19 09:31:52 +0200

    Commit 1

---
+++ Practice.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+diamonds2<-readRDS("diamonds2.rds")
+
+
+diamonds2 %>% 
+  pivot_longer(cols      = c("2008", "2009"), 
+               names_to  = 'year', 
+               values_to = 'price') %>% 
+  head(n = 5)
+
